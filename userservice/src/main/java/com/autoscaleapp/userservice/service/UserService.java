ada2ab42a5aa304f3316751c8a3ffba549389efd
XSuhaniSinghX <115005183+XSuhaniSinghX@users.noreply.github.com>
2025-03-08 18:29:48 +0530

    this one's for registery etc etc.

---
+++ userservice/src/main/java/com/autoscaleapp/userservice/service/UserService.java
@@ -0,0 +1,29 @@
+package com.autoscaleapp.userservice.service;
+
+import com.autoscaleapp.userservice.model.User;
+import com.autoscaleapp.userservice.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+//third step y'all
+// this one's the real business logic
+
+import java.util.Optional;
+
+@Service //real service. seperates business logic from controllers
+//keeps code as clean as low taper fade
+public class UserService {
+    
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    } //for some reason they call it constructor injection 
+
+    public User registerUser(User user) {
+        return userRepository.save(user);
+    }//saving user here , well not exactly here, but in db
+
+    public Optional<User> findByUsername(String username) {//to handle nullpointer exception. too many exception these days.
+        return userRepository.findByUsername(username);
+    }
+}
