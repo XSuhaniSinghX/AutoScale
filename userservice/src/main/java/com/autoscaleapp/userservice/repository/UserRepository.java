88dec28a5e4f33b065ece4fb9c6621fdf890530e
XSuhaniSinghX <115005183+XSuhaniSinghX@users.noreply.github.com>
2025-03-08 18:19:30 +0530

    done with the crud too. i'm so productive

--- userservice/src/main/java/com/autoscaleapp/userservice/repository/UserRepository.java
+++ userservice/src/main/java/com/autoscaleapp/userservice/repository/UserRepository.java
@@ -1,11 +1,12 @@
 package com.autoscaleapp.userservice.repository;
+// step 2 right here
+//this one will handle crud operations 
 
-
-import com.autoscalepro.userservice.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.autoscaleapp.userservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;//main stuff, will handle all crud ops.
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
-    Optional<User> findByEmail(String email);
+    Optional<User> findByEmail(String email);//giving both options caz i am cool.
 }
